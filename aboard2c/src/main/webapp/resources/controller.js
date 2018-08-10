5f4ca32c8d5a798101c74261c9f76e507b2a3683
snrntpa2z <snrntpa2z@naver.com>
2018-08-10 15:27:05 +0900

    18-08-10 오후 3:26
    1차 리팩토링
    게시판 리스트, 상세정보, 댓글 <<
    ==============================

--- aboard2c/src/main/webapp/resources/controller.js
+++ aboard2c/src/main/webapp/resources/controller.js
@@ -1,55 +1,42 @@
 //boardList
-app.controller('boardsCtrl',function($scope, $http,$routeParams){
+app.controller('boardsCtrl',function($scope, $http,$routeParams,myStorage){
 	// boards 리스트 REST방식으로 page값을 넘긴후 데이터를 가져와서 boardsList에 담는다.
 	var page = $routeParams.page;
-	 $http.get("/aboard2/boards/list/"+page).then(function(response){
-			$scope.boardsList = JSON.parse(response.data.records);
-		});
-	 
-	 $scope.searchFuc=function(){
-		 $http.get("/aboard2/boards/listAll").then(function(response){
-				$scope.listAll = JSON.parse(response.data.listAll); 
-			 });
+	// 게시판 리스트 메소드
+	myStorage.get(page).then(function(data){
+		$scope.boardsList = data;
+	});
+	// 게시판 검색 메소드
+	 $scope.searchFuc = function(search){
+		 myStorage.listAll().then(function(data){
+		 $scope.listAll = data;
+		 });
+		 
 	 };
-	 
   });
 // boardRead
-app.controller('boardsReadCtrl',function($scope,$http,$routeParams){
+app.controller('boardsReadCtrl',function($scope,$http,$routeParams,myStorage){
 	// routeParams로 Query의 값을 받을 수 있다.
-	// 그 값을 GET 방식 Ajax비동기 통신으로 전달해서 resp변수에 값을 저장한다.
 	var bno = $routeParams.bno;
-	// ReplyList
-	function replyList(){$http.get("/aboard2/boards/reply/list?bno="+bno)
-		.then(function(response){
-			$scope.list = response.data.list;
-			
-		});
+	$scope.bno = bno;
+	var rText = $scope.rText;
+	// 게시판 글 상세정보
+	myStorage.boardsRead(bno).then(function(data){
+		myStorage.replyList(bno);
+		$scope.resp = data;
+	});
+	// 댓글 리스트
+	myStorage.replyList(bno).then(function(data){
+		$scope.list = data;
+	});
+	// 댓글 작성
+	$scope.myFuc = function(rText,bno){
+		myStorage.replyInsert(rText,bno).then(function(data){
+			myStorage.replyList(bno);
+			$scope.rText = "";
+			alert(data);
+	});
 		};
-	// ReplyList
-	$http.get("/aboard2/boards/read/"+bno)
-	.then(function(response){
-		replyList();
-		$scope.resp = response.data;
-	});
-	// ReplyInsert
-	$scope.myFuc=function(){
-		// ng-click 시 작동 되는 함수이다.
-		// myFuc으로 reply 라는 Object를 컨트롤러에 값을 넘겨준다
-		var data = {"replytext":$scope.rText,"bno":bno,"id":"service"};
-		$http({
-		url : '/aboard2/boards/reply/insert',
-		method : 'post',
-		data: "reply="+JSON.stringify(data),
-		contentType:"application/json;charset=UTF-8",
-		headers : {'Content-Type': 'application/x-www-form-urlencoded'},
-
-	}).then(function(response){
-		// insert가 성공했을 때
-		replyList();
-		$scope.rText="";
-		alert('zz');
-	});
-	};
 });
 // UserController 부분
 app.controller('registerCtrl', function($scope, $http,$window,$location){
