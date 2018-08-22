a11ad4218e5f5a89c0add8589411ded4a900749e
snrn <snrntp>
2018-08-22 15:33:09 +0900

    08--22
    나만의 여행 계획짜는 기능
    만들예정

---
+++ aboard2c/src/main/webapp/resources/travelDirectives.js
@@ -0,0 +1,30 @@
+//Input에서 ng-model을 사용했을때 한글 dataBinding을 해주는 directive
+
+app.directive('krInput', [ '$parse', function($parse) { return { priority : 2, restrict : 'A', compile : function(element) { element.on('compositionstart', function(e) { e.stopImmediatePropagation(); }); }, }; } ])
+.config(function($routeProvider){
+	$routeProvider
+	.when("/mytravel/list",{
+		templateUrl:"./travel/list.html",
+		controller:"travleCtrl"
+	});
+
+	
+})
+.config(['$qProvider', function ($qProvider) {
+    $qProvider.errorOnUnhandledRejections(false);
+}])
+.run(['$rootScope', '$location', '$cookieStore', '$http',
+    function ($rootScope, $location, $cookieStore, $http) {
+        // keep user logged in after page refresh
+        $rootScope.globals = $cookieStore.get('globals') || {};
+        if ($rootScope.globals.currentUser) {
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
+        }
+ 
+        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+            // redirect to login page if not logged in
+            if ($location.path() !== '/users/login' && !$rootScope.globals.currentUser) {
+                $location.path('/users/login');
+            }
+        });
+    }]);
